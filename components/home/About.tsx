fccb626d9b0bb453a4747ff0a383c7ce44f5a91d
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-12 01:29:27 +0900

    feat: :iphone: ABOUTページのレスポンシブ対応

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -9,24 +9,24 @@ const About = ({ changeTab }: Props) => {
   return (
     <div>
       <Header changeTab={changeTab} />
-      <div className="px-4">
+      <div className="px-4 md:px-40">
         <h2 className="text-3xl md:text-4xl">ABOUT</h2>
         <section className="px-2 py-2">
-          <h3 className="text-xl md:text-2xl">Profile</h3>
-          <div className="px-2 text-sm">
+          <h3 className="text-2xl md:text-3xl mb-2">Profile</h3>
+          <div className="px-2 text-sm md:text-base">
             <p>青柳 仁 Aoyagi Jin</p>
             <p>慶應義塾大学理工学部電気情報工学科2年</p>
           </div>
         </section>
         <section className="px-2 py-2">
-          <h3 className="text-xl md:text-2xl mb-2">History</h3>
-          <div className="flex flex-col gap-2 px-2 text-xs">
+          <h3 className="text-2xl md:text-3xl mb-2">History</h3>
+          <div className="flex flex-col gap-2 px-2 text-sm md:text-base">
             <div>
-              <p>2021/04</p>
+              <p className="font-bold">2021/04</p>
               <p className="pl-4">慶應義塾大学理工学部 入学</p>
             </div>
             <div>
-              <p>2021/06-2023/03</p>
+              <p className="font-bold">2021/06-2023/03</p>
               <p className="pl-4">
                 プログラミング学習コミュニティPOSSEに所属. JavaScript, PHP,
                 Laravel, MySQLなどを使用したWebアプリケーション開発を学んだ.
@@ -34,7 +34,7 @@ const About = ({ changeTab }: Props) => {
               </p>
             </div>
             <div>
-              <p>2022/07-2023/04</p>
+              <p className="font-bold">2022/07-2023/04</p>
               <p className="pl-4">
                 株式会社アンチパターンに所属.
                 長期インターン生として受託開発のプロダクトに携わり,
@@ -42,7 +42,7 @@ const About = ({ changeTab }: Props) => {
               </p>
             </div>
             <div>
-              <p>2022/09-</p>
+              <p className="font-bold">2022/09-</p>
               <p className="pl-4">
                 学生火星探査ローバー開発チームARES Projectに所属.
                 広報局の一員としてチームのWebサイトを制作, 運営する.
@@ -51,8 +51,8 @@ const About = ({ changeTab }: Props) => {
           </div>
         </section>
         <section className="px-2 py-2">
-          <h3 className="text-xl md:text-2xl">Skills</h3>
-          <ul className="text-xs px-2 w-2/3">
+          <h3 className="text-2xl md:text-3xl mb-2">Skills</h3>
+          <ul className="text-sm md:text-base px-2 w-2/3">
             <li className="flex justify-between">
               <span>HTML5/CSS3</span>
               <span>■■■■■■■■□□</span>
