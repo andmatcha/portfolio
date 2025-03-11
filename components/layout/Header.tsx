4b011b6c3802848095a527229a017e53480047c6
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-11 15:32:18 +0900

    feat: :iphone: ヘッダーのレスポンシブ対応

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const Header = ({ changeTab }: Props) => {
   return (
-    <header className="w-full h-20 px-10 flex justify-end items-center">
-      <ul className="flex gap-8">
+    <header className="w-full h-16 md:h-20 px-4 md:px-10 flex justify-end items-center text-sm md:text-base">
+      <ul className="flex gap-4 md:gap-8">
         <li className="cursor-pointer" onClick={() => changeTab("top")}>TOP</li>
         <li className="cursor-pointer" onClick={() => changeTab("about")}>ABOUT</li>
         <li className="cursor-pointer" onClick={() => changeTab("works")}>WORKS</li>
