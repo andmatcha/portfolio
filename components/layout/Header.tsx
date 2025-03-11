1b9109bf53f3412947e61db6c0066c7d1d43684f
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-12 04:21:36 +0900

    feat: :lipstick: ボタンにホバーエフェクトを追加

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -8,10 +8,30 @@ const Header = ({ changeTab }: Props) => {
   return (
     <header className="fixed w-full h-16 md:h-20 px-4 md:px-10 flex justify-end items-center text-sm md:text-base">
       <ul className="flex gap-4 md:gap-8">
-        <li className="cursor-pointer" onClick={() => changeTab("top")}>TOP</li>
-        <li className="cursor-pointer" onClick={() => changeTab("about")}>ABOUT</li>
-        <li className="cursor-pointer" onClick={() => changeTab("works")}>WORKS</li>
-        <li className="cursor-pointer" onClick={() => changeTab("contact")}>CONTACT</li>
+        <li
+          onClick={() => changeTab("top")}
+          className="cursor-pointer hover:text-sky-500 transition-all duration-300"
+        >
+          TOP
+        </li>
+        <li
+          onClick={() => changeTab("about")}
+          className="cursor-pointer hover:text-sky-500 transition-all duration-300"
+        >
+          ABOUT
+        </li>
+        <li
+          onClick={() => changeTab("works")}
+          className="cursor-pointer hover:text-sky-500 transition-all duration-300"
+        >
+          WORKS
+        </li>
+        <li
+          onClick={() => changeTab("contact")}
+          className="cursor-pointer hover:text-sky-500 transition-all duration-300"
+        >
+          CONTACT
+        </li>
       </ul>
     </header>
   );
